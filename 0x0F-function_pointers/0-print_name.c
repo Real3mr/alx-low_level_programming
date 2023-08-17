9327261592a6f782f3edcb63d2663ae41a646c45
Real3mr <me@example.com>
2023-08-17 15:46:37 +0300

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_name - using pointer to function
- * @name
+ * print_name - print name using pointer to function
+ * @name: string to add
  * @f: pointer to function
- *
+ * Return: nothing
  **/
 void print_name(char *name, void (*f)(char *))
 {
@@ -13,4 +13,3 @@ void print_name(char *name, void (*f)(char *))
 
 	f(name);
 }
-
